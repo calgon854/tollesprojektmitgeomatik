c0bce06aeb432750ba8517091d1b2cacea225e1a
Ignaz Kuczynski <ignaz.kuczynski@gmail.com>
2025-01-02 11:29:49 +0100

    checks

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@
 //   );
 // }
 import React from "react";
+import "../styles/style.css";
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- pages/info.js
+++ pages/info.js
@@ -8,6 +8,15 @@
 
 import React, { useState, useEffect } from "react";
 import { AppBar, Toolbar, Button } from "@mui/material";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import dynamic from "next/dynamic";
+
+const MapRender = dynamic(() => import("../components/maps/QuakeMap.jsx"), {
+  ssr: false,
+});
 
 export default function Index() {
   const [activeTab, setActiveTab] = useState("Tab1");
@@ -35,6 +44,24 @@ export default function Index() {
             <h1>Tab 2</h1>
           </>
         )}
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar elevation={0} position="static" sx={{ bgcolor: "#000000" }}>
+            <Toolbar variant="regular">
+              <IconButton
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" color="inherit" component="div">
+                USGS Earthquakes
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </Box>
+        <MapRender />
       </div>
     </>
   );
